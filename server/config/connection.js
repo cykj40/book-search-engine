355422a5a7b6988f90abab6bdda2f8dd3e7e9b27
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-02-24 18:08:49 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cyrusk41:CKJ7630542h@googlebooks.tpnx6nk.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
